b9cc808b1f7ab3f107ae44c3dcbffab7261fee96
Gerald_TM <99144289+GeraldTM@users.noreply.github.com>
2022-04-10 16:40:31 -0400

    Start motorLoader class, import Phoenix, and create ExampleMotorConfig file

---
+++ WIPLib 2022 Template/src/main/java/frc/robot/subsystems/motorLoader.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.talon.TalonSRX;
+import com.ctre.ctre.motorcontrol.can.victor.VictorSPX;
+
+public class motorLoader {
+    // Declare motor variables
+    TalonSRX talonMotor;
+    VictorSPX victorMotor;
+
+}
