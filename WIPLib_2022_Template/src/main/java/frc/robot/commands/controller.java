87ef2c2b5af410b58fe1daf5c53c3ccabd123711
Wired7475FRC <46526354+Wired7475FRC@users.noreply.github.com>
2022-05-17 16:56:12 -0400

    create controller class and conceptualize

---
+++ WIPLib_2022_Template/src/main/java/frc/robot/commands/controller.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import java.lang.reflect.Method;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class controller {
+    static String filePath = "";
+    public controller(){
+
+    }
+    public static void setControllerMappingPath(String path) {
+        filePath = path;
+    }
+
+
+
+    public void bind(Method method){
+
+    }
+}
